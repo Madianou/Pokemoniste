6bc28c7d80dce3967b4b0a9fe46878aca8584646
RaphaelDiMascio <raphou095@gmail.com>
2022-05-23 22:31:17 +0200

    création de la page d'accueil sans css

--- src/main/java/com/example/pokemoniste/HelloApplication.java
+++ src/main/java/com/example/pokemoniste/HelloApplication.java
@@ -9,6 +9,7 @@ import modele.GrapheOriente;
 import modele.Membres;
 import modele.Scenario;
 import vue.HBoxTest;
+import vue.PageAccueil;
 
 import java.io.File;
 import java.io.IOException;
@@ -20,12 +21,12 @@ public class HelloApplication extends Application {
         Carte carte = new Carte();
         Membres membres = new Membres();
         Scenario scenario = Scenario.lectureScenario(new File("src/files/scenario/scenario_2_1.txt"));
-        GrapheOriente test = new GrapheOriente(scenario,carte,membres);
-        System.out.println(test);
-        HBoxTest root = new HBoxTest();
+        //GrapheOriente test = new GrapheOriente(scenario,carte,membres);
+        //System.out.println(test);
+        PageAccueil root = new PageAccueil();
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(root, 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(root, 600, 400);
+        stage.setTitle("Page d'accueil");
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/vue/PageAccueil.java
@@ -0,0 +1,40 @@
+package vue;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+
+public class PageAccueil extends VBox {
+    private static Image chImage;
+    private static Button chBouton;
+
+    public PageAccueil(){
+
+        HBox boxPokemon = new HBox(10);
+        HBox boxBouton = new HBox(10);
+
+        final ImageView imageView = new ImageView();
+
+        String url = "C:\\Users\\Raphaël\\Documents\\Ecole\\2e semestre\\Saé\\Saé Commune - Dev\\Pokemoniste\\Picture\\Pokemon.png";
+        //Il faut changer le chemin pour pouvoir accéder à l'image.
+
+        chImage = new Image(url);
+        imageView.setImage(chImage);
+        imageView.setFitHeight(300);
+        imageView.setFitWidth(600);
+
+        boxPokemon.getChildren().add(imageView);
+
+        chBouton = new Button("Accéder à l'application");
+        chBouton.setAlignment(Pos.CENTER_RIGHT);
+        boxBouton.setHgrow(chBouton, Priority.ALWAYS);
+        boxBouton.getChildren().add(chBouton);
+
+
+        this.getChildren().addAll(boxPokemon, boxBouton);
+    }
+}
