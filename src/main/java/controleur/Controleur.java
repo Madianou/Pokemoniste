e5b02facb0b579466b889e68d978610334183bfb
RaphaelDiMascio <99277064+RaphaelDiMascio@users.noreply.github.com>
2022-06-20 13:41:14 +0200

    modification page présentation

--- src/main/java/controleur/Controleur.java
+++ src/main/java/controleur/Controleur.java
@@ -4,10 +4,15 @@ import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
+import modele.Carte;
 import modele.ConstantesPokemoniste;
+import modele.GrapheOriente;
+import modele.Membres;
 import vue.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Controleur implements EventHandler, ConstantesPokemoniste {
     private static String chUrlScenario = URL_SCENARIO[0];
@@ -19,16 +24,26 @@ public class Controleur implements EventHandler, ConstantesPokemoniste {
         PageFrance france = VBoxRoot.getFrance();
         PageAccueil accueil = VBoxRoot.getAccueil();
 
-        if (event.getSource() instanceof MenuItem){
+
+        if (event.getSource() instanceof MenuItem) {
             chUrlScenario = URL_SCENARIO[(int) ((MenuItem) event.getSource()).getUserData()];
+            Carte carte;
+            Membres membres;
             try {
                 presentation.setUrlScenario(chUrlScenario);
                 france.setUrlScenario(chUrlScenario);
+                carte = new Carte();
+                membres = new Membres();
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
             System.out.println(presentation.getScenario());
             membre.setMembres(presentation.getScenario().getMembresString(), presentation.getScenario().getAcheteursString(), presentation.getScenario().getVendeursString());
+
+            GrapheOriente graphe = new GrapheOriente(presentation.getScenario(), carte, membres);
+            List<List<Integer>> chaine = graphe.sourceComplet(28,new ArrayList<List<Integer>>(),new ArrayList<Integer>(),graphe.getSource(),graphe.getDegreEntrant());
+            presentation.setTextChemin(graphe.toStringSimplifié());
+            presentation.setTextCheminBref(graphe.BestChemin(chaine, carte));
         }
         if (event.getSource() instanceof Button){
             if (((Button) event.getSource()).getId().equals("Accueil")){
--- src/main/java/vue/PagePrésentation.java
+++ src/main/java/vue/PagePrésentation.java
@@ -8,21 +8,22 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
-import modele.ConstantesPokemoniste;
-import modele.Scenario;
+import modele.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PagePrésentation extends HBox implements ConstantesPokemoniste {
     private static TextArea chCheminBref;
-    private static ScrollPane chMembre;
-    private static ScrollPane chChemin;
+    private static TextArea chMembre;
+    private static TextArea chChemin;
     private static Button chBouton;
     private static String chUrlScenario = URL_SCENARIO[0];
     private static Scenario chScenario;
 
-    public PagePrésentation() throws IOException {
+    public PagePrésentation() throws IOException{
         this.setSpacing(ESPACEMENT);
         Controleur controleur = VBoxRoot.getControleur();
         chScenario = Scenario.lectureScenario(new File(chUrlScenario));
@@ -34,15 +35,22 @@ public class PagePrésentation extends HBox implements ConstantesPokemoniste {
         VBoxDroite.setMinWidth(LARGEUR/2);
         VBoxDroite.setSpacing(ESPACEMENT);
 
+        Carte carte = new Carte();
+        Membres membres = new Membres();
+        GrapheOriente graphe = new GrapheOriente(chScenario, carte, membres);
+        List<List<Integer>> chaine = graphe.sourceComplet(28,new ArrayList<List<Integer>>(),new ArrayList<Integer>(),graphe.getSource(),graphe.getDegreEntrant());
+
         chCheminBref = new TextArea();
-        chMembre = new ScrollPane();
+        chCheminBref.setText(graphe.BestChemin(chaine, carte));
+        chMembre = new TextArea();
 
         VBoxGauche.getChildren().addAll(chCheminBref, chMembre);
 
-        chChemin = new ScrollPane();
+        chChemin = new TextArea();
+        chChemin.setText(graphe.toStringSimplifié());
 
         HBox bouton = new HBox();
-        chBouton = new Button("Bouton vers France");
+        chBouton = new Button("Bouton vers la carte de France");
         chBouton.setId("France");
         chBouton.setOnAction(controleur);
         bouton.getChildren().add(chBouton);
@@ -60,5 +68,14 @@ public class PagePrésentation extends HBox implements ConstantesPokemoniste {
     public static Scenario getScenario(){
         return chScenario;
     }
+    public static void setTextChemin(String string){
+        chChemin.clear();
+        chChemin.setText(string);
+    }
+    public static void setTextCheminBref(String string){
+        chCheminBref.clear();
+        chCheminBref.setText(string);
+    }
+
 
 }
